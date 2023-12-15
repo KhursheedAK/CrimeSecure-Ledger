78f6535173dd39dda8b287872935f93fd7a4a839
i200496-Khursheed <i200496@nu.edu.pk>
2023-12-15 19:40:30 +0500

    Latest V1.0 Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # CrimeSecure-Ledger
 A simple MERN based block chain web application that allows admin to add criminal records in ledger to prevent data temper. Citizens can only view the added criminal records. Record is added both in MongoDB and in Blockchain (Ganache)
+
+Technology Used:
+• Node.js
+• Express
+• Truffle
+• Ganache
+
+• React.js
+• Web3
---
+++ backend/contracts/criminalRecord.sol
@@ -0,0 +1,25 @@
+// contracts/CriminalRecord.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CriminalRecord {
+    struct Crime {
+        string name;
+        string crime;
+        uint256 age;
+        string status;
+        string image;
+    }
+
+    mapping(uint256 => Crime) public crimes;
+    uint256 public crimeCount;
+
+    constructor() {
+        crimeCount = 0;
+    }
+
+    function addCrime(string memory name, string memory crime, uint256 age, string memory status, string memory image) public {
+        crimeCount++;
+        crimes[crimeCount] = Crime(name, crime, age, status, image);
+    }
+}
